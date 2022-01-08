31db403345a43bac7223f55e21aca16eec657233
SpeedFox198 <65378401+SpeedFox198@users.noreply.github.com>
2022-01-08 17:34:16 +0800

    added route to signup page

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import shelve
 
 # Import classes
 from users import User, Customer, Admin, Master
+from forms import SignUpForm
 
 app = Flask(__name__)
 
@@ -14,6 +15,13 @@ def home():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    sign_up_form = SignUpForm(request.form)
+    if request.method == 'POST' and sign_up_form.validate():
+        return redirect(url_for('home'))
+    return render_template('sign_up.html', form=sign_up_form)
 
 # Only during production
 # To be removed when published
