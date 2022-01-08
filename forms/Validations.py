b948b9cbe17d90a2557a21a5f39c82c8356a7d19
SpeedFox198 <65378401+SpeedFox198@users.noreply.github.com>
2022-01-08 13:26:13 +0800

    Upgraded Validation (uses regex)

--- forms/Validations.py
+++ forms/Validations.py
@@ -4,65 +4,62 @@ Validation classs used by Forms.py
 P.S. Teacher I read through the source code for wtforms.validators
 to figure out how does the validators class worked to create
 my own custom validator class Contains
+I also went and learn how to use regex
 gimme extra marks pls
 """
 from wtforms import ValidationError
+import re
 
 
-class Check:
+class SearchRegex:
     """
-    Checks the incoming data using the given function.
+    Search the incoming data using the given regex pattern
 
     Args:
-        function (function): A function to check if data is valid.
-        message (str): Error message to raise in case of a validation error.
+        pattern (str): The regex pattern
+        message (str): Error message to raise in case of a validation error
     """
 
-    def __init__(self, function, message):
-        self.__function = function
+    def __init__(self, pattern, message):
+        self.__pattern = re.compile(pattern)
         self.__message = message
 
     def __call__(self, form, field):
-        for c in field.data:
-            if self.__function(c):
-                return
+        if self.__pattern.search(field.data):
+            return
 
         raise ValidationError(self.__message)
 
 
-class ContainsLower(Check):
+class ContainsLower(SearchRegex):
     """
     Checks if data incoming data contains a lowercase character
     """
 
     def __init__(self):
-        super().__init__(lambda character: "a" <= character <= "z",
+        super().__init__(r"[a-z]",
                          "Field must contain at least one lowercase letter.")
 
 
-class ContainsUpper(Check):
+class ContainsUpper(SearchRegex):
     """
     Checks if data incoming data contains an uppercase character
     """
 
     def __init__(self):
-        super().__init__(lambda character: "A" <= character <= "Z",
+        super().__init__(r"[A-Z]",
                          "Field must contain at least one uppercase letter.")
 
 
-class ContainsNumSymbol(Check):
+class ContainsNumSymbol(SearchRegex):
     """
     Checks if data incoming data contains a number or symbol
     """
 
     def __init__(self):
-        super().__init__(self.__check,
+        # numbers:      [0-9]
+        # symbols: [ -/]     [:-@]     [\[-1]     [{-~]
+        # letters:                [A-Z]      [a-z]
+        # num&sym: [      -     @]     [\[-1]     [{-~]
+        super().__init__(r"[ -@\[-`{-~]",
                          "Field must contain at least one symbol or number.")
-    
-    @staticmethod
-    def __check(character):
-        """ Checks if character is number or symbol """
-        value = ord(character)
-        # value of int: 48-57
-        # value of sym: 32-47, 58-64, 91-86, 123-126
-        return 32<=value<=64 or 91<=value<=96 or 123<=value<=126
