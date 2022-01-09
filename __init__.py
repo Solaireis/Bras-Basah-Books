55507e05a4a8b09f8dab76a921ae47a7acf06f59
SpeedFox198 <65378401+SpeedFox198@users.noreply.github.com>
2022-01-09 20:41:59 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,40 +6,44 @@ import shelve
 
 # Import classes
 from users import User, Customer, Admin, Master
-from forms import SignUpForm
+from forms import SignUpForm, SetPasswordForm
 
 
 app = Flask(__name__)
-app.config.from_pyfile("app.cfg")  # Load config file
+app.config.from_pyfile("config/app.cfg")  # Load config file
 
-# Serialiser for generating tokens
-url_serialiser = URLSafeTimedSerializer(app.config["SECRET_KEY"])
+url_serialiser = URLSafeTimedSerializer(app.config["SECRET_KEY"]) # Serialiser for generating tokens
 
 mail = Mail()  # Mail object for sending emails 
 
 
+# Home page
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
+# Login page
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+
+# Sign up page
 @app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     sign_up_form = SignUpForm(request.form)
     if request.method == "POST" and sign_up_form.validate():
         
         # Configure noreplybbb02@gmail.com
-        app.config.from_pyfile("noreply_email.cfg")
+        app.config.from_pyfile("config/noreply_email.cfg")
         mail.init_app(app)
 
         # Extract email value from sign up form
         email = request.form["email"]
 
         # Generate token
-        token = url_serialiser.dumps(email, salt="sign-up")
+        token = url_serialiser.dumps(email, salt=app.config["SIGN_UP_SALT"])
 
         # Send message to email entered
         msg = Message(subject="Verify Email",
@@ -51,34 +55,62 @@ def sign_up():
 
         return f"email: {email}, token: {token}"
 
+    # Render page
     return render_template("sign_up.html", form=sign_up_form)
 
-@app.route("/sign-up/verify/<token>")
+
+# Sign up verify email page
+@app.route("/sign-up/verify/<token>", methods=["GET", "POST"])
 def sign_up_verify(token):
-    try:  # Get email from token
-        email = url_serialiser.loads(token, salt="sign-up", max_age=900)
+
+    # Get email from token
+    try:
+        email = url_serialiser.loads(token, salt=app.config["SIGN_UP_SALT"], max_age=900)
     except SignatureExpired:  # Token expired
         return "The token expired!"
-    except (BadTimeSignature, BadSignature):  # Invalid token
-        return ("<h1>Unable to verify email address</h1>"
-                "Your email address could not be verified, "
-                "this link may be expired or malformed.")
-    return "Token works!"
+    except (BadTimeSignature, BadSignature) as err:  # Invalid token
+        print(repr(err))  # print captured error
+        return "Oops - This link is invalid or expired."
+
+    # Set password
+    set_password_form = SetPasswordForm(request.form)
+    if request.method == "POST" and set_password_form.validate():
+
+        # Create new user
+        with shelve.open("database") as db:
+
+            # Get Customers and EmailToUserID
+            try:
+                customers_db = db["Customers"]
+            except KeyError:  # Customers not created yet
+                customers_db = {}
+            try:
+                email_to_user_id = db["EmailToUserID"]
+            except KeyError:  # EmailToUserID not created yet
+                email_to_user_id = {}
+
+            customer = Customer(email, request.form["password"])
+
+            # Store customer into database
+            user_id = customer.get_user_id()
+
+            customers_db[user_id] = customer
+            db["Customers"] = customers_db
+
+            email_to_user_id[email] = user_id
+            db["EmailToUserID"] = email_to_user_id
+
+        return "user created"
+
+    # Render page
+    return render_template("sign_up_verify.html", form=set_password_form)
 
 
-# with shelve.open("database") as db:
-#     try:
-#         customers_db = db["customers"]
-#     except KeyError:
-#         print('No "customers" key in database')
-#         customers_db = {}
-#     customer = Customer(sign_up_form.email)
-
-# Only during production
-# To be removed when published
-@app.route("/test")  # To go to test page 127.0.0.1:5000/test
+# Only during production. To be removed when published.
+@app.route("/test")  # To go to test page: http://127.0.0.1:5000/test
 def test():
     return render_template("test.html")
+
 
 if __name__ == "__main__":
     app.run()
