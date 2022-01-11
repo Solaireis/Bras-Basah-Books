69320c7179d9beff8b928bc03124ce928abc4b2e
SpeedFox198 <65378401+SpeedFox198@users.noreply.github.com>
2022-01-11 12:54:34 +0800

    Add files via upload

--- users/Admin.py
+++ users/Admin.py
@@ -8,7 +8,7 @@ class Admin(Customer):
         __user_id (str): unique ID identifier of admin
         __email (str): email of admin
         __password (str): hashed password of admin
-        __name (str): name of admin
+        __username (str): username of admin
         __profile_pic (str): path of profile pic of admin
         __gender (str): gender of admin - "M" for male, "F" for female, "O" for other
         __coupons (list): not used by admin
@@ -16,5 +16,5 @@ class Admin(Customer):
         shopping_cart (ShoppingCart): not used by admin
     """
 
-    def __init__(self, email, password):
-        super().__init__(email, password)
+    def __init__(self, email, password, username=""):
+        super().__init__(email, password, username)
--- users/User.py
+++ users/User.py
@@ -1,3 +1,4 @@
+import uuid
 # from ShoppingCart import ShoppingCart
 
 class User:
@@ -5,8 +6,13 @@ class User:
     User account for guests (anonymous users)
 
     Attributes:
+        __user_id (str): unique ID identifier of user
         shopping_cart (ShoppingCart): shopping cart of user
     """
 
     def __init__(self):
+        self.__user_id = str(uuid.uuid4())
         self.shopping_cart = None  # ShoppingCart()
+
+    def get_user_id(self):
+        return self.__user_id
