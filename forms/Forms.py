c37f29d95d254c044dee6b30646e79afa23d23df
SpeedFox198 <65378401+SpeedFox198@users.noreply.github.com>
2022-01-13 20:29:25 +0800

    testing (to be deleted)

--- forms/Forms.py
+++ forms/Forms.py
@@ -18,15 +18,15 @@ class SignUpForm(Form):
 
     # Password
     password = PasswordField("Password", [validators.InputRequired(),
-                                              validators.Length(min=8, max=80),
-                                              ContainsLower(), ContainsUpper(),
-                                              ContainsNumSymbol()])
+                                          validators.Length(min=8, max=80),
+                                          ContainsLower(), ContainsUpper(),
+                                          ContainsNumSymbol()])
 
     # Confirm password
     confirm = PasswordField("Confirm Password", [validators.EqualTo("password")])
 
     # Username
-    username = StringField("Username", [validators.Optional()])
+    username = StringField("Username", [validators.Length(max=25)])
 
 
 class LoginForm(Form):
@@ -37,3 +37,13 @@ class LoginForm(Form):
 
     # Password
     password = PasswordField("Password", [validators.InputRequired()])
+
+
+class AccountPageForm(Form):
+    """ Account page form used for editing account """
+
+    # Username
+    username = StringField("Name", [validators.Length(max=25)])
+
+    # Gender
+    gender = RadioField("Gender", choices=[("M", "Male"), ("F", "Female"), ("O", "Others")])
--- users/__init__.py
+++ users/__init__.py
@@ -10,8 +10,10 @@ Master (master admin account) (child class of Admin)
 """
 # Why did I do this? I dunno, thought it was funny HAHAHA
 from .User import User as User
+from .Guest import Guest as Guest
 from .Customer import Customer as Customer
 from .Admin import Admin as Admin
 from .Master import Master as Master
+from .GuestDB import GuestDB as GuestDB
 # Please don't mark us down for "unclean code"
 # Just treat this as an easter egg
