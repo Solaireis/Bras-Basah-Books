fd51a4677bd982720424656d73d8a3dee22bfba2
Eden <64953425+Solaireis@users.noreply.github.com>
2022-01-18 10:45:46 +0800

    Add files via upload

--- forms/Reply.py
+++ forms/Reply.py
@@ -1,8 +1,16 @@
 import uuid
-from .Enquiry import Enquiry
+from .Enquiry import UserEnquiry
 
-class Reply(Enquiry):
+class Reply(UserEnquiry):
     def __init__(self, reply):
-        self.reply_id = uuid.uuid4()  # unique id for the reply
-        self.reply = reply
+        self.__reply_id = uuid.uuid4()  # unique id for the reply
+        self.__reply = reply
 
+    def get_reply_id(self):
+        return self.__reply_id
+
+    def get_reply(self):
+        return self.__reply
+
+    def set_reply(self,reply):
+        self.__reply = reply
