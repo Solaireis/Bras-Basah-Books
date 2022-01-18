3cf371bfdb14c8f281277ca10982012391876503
Eden <64953425+Solaireis@users.noreply.github.com>
2022-01-18 22:33:53 +0800

    Add files via upload

--- forms/__init__.py
+++ forms/__init__.py
@@ -1,3 +1,4 @@
-from .Enquiry import Enquiry
+from .Enquiry import Enquiry, UserEnquiry
 from .Reply import Reply
 from .Forms import SignUpForm, LoginForm, AccountPageForm
+from .Faq import Faq, FaqEntry
