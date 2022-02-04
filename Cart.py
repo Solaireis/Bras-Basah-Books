aafeadc8a77e46ca642ab8a6404da0414d50d8a4
Kelly <kelkytan2002@gmail.com>
2022-02-04 14:23:33 +0800

    Update Cart.py

--- Cart.py
+++ Cart.py
@@ -1,3 +1,28 @@
+
+class SCart():
+    buy_dict = {}
+    rent_dict = {}
+
+
+
+class AddtoBuy(SCart):
+    def __init__(self, book_id, book_quantity):
+        Cart.__init__(self, book_id, book_quantity)
+        self.buy_dict.update({book_id:book_quantity})
+        print(self.buy_dict)
+
+    def get_buy_dict(self):
+        return self.buy_dict
+
+    def get_book_id(self):
+        return self.book_id
+
+class AddtoRent(SCart):
+    def __init__(self, book_id, book_quantity):
+        Cart.__init__(self)
+        self.rent_dict.update({book_id:book_quantity})
+        print(self.rent_dict)
+
 class Cart():
     def __init__(self, book_id, book_quantity):
         self.book_id = book_id
@@ -21,3 +46,7 @@ class RentCart():
         return self.book_id
     def set_book_id(self, book_id):
         self.book_id = book_id
+
+    testing = {'s':{'a':'c'}}
+    testing['s'].update({'c':'c'})
+    print(testing)
