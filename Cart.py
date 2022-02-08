eec30f9c3cfd5efbb2ce4122f1aba0cc5cea7377
Kelly <kelkytan2002@gmail.com>
2022-02-08 19:02:01 +0800

    Update Cart.py

--- Cart.py
+++ Cart.py
@@ -1,13 +1,13 @@
 
 class SCart():
     buy_dict = {}
-    rent_dict = {}
-
-
+    rent_dict = []
 
 class AddtoBuy(SCart):
     def __init__(self, book_id, book_quantity):
-        Cart.__init__(self, book_id, book_quantity)
+        SCart.__init__(self)
+        self.book_id = book_id
+        self.book_quantity = book_quantity
         self.buy_dict.update({book_id:book_quantity})
         print(self.buy_dict)
 
@@ -18,35 +18,15 @@ class AddtoBuy(SCart):
         return self.book_id
 
 class AddtoRent(SCart):
-    def __init__(self, book_id, book_quantity):
-        Cart.__init__(self)
-        self.rent_dict.update({book_id:book_quantity})
+    def __init__(self, book_id):
+        SCart.__init__(self)
+        self.rent_dict.append(book_id)
         print(self.rent_dict)
 
-class Cart():
-    def __init__(self, book_id, book_quantity):
-        self.book_id = book_id
-        self.book_quantity = book_quantity
+    def get_rent_dict(self):
+        return self.rent_dict
 
     def get_book_id(self):
         return self.book_id
-    def set_book_id(self, book_id):
-        self.book_id = book_id
 
-    def get_book_quantity(self):
-        return self.book_quantity
-    def set_book_id(self, book_quantity):
-        self.book_quantity = book_quantity
 
-class RentCart():
-    def __init__(self, book_id):
-        self.book_id = book_id
-
-    def get_book_id(self):
-        return self.book_id
-    def set_book_id(self, book_id):
-        self.book_id = book_id
-
-    testing = {'s':{'a':'c'}}
-    testing['s'].update({'c':'c'})
-    print(testing)
