2abfc728c3dd9aa0d637312b79b20b7309095e1d
Kelly <kelkytan2002@gmail.com>
2022-02-11 23:16:12 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -532,13 +532,20 @@ def manage_accounts():
 """|____________________________________________|"""
 
 
+#
+# Start of Chee Qing's Codes
+#
+
+#
 # allbooks
+#
 @app.route("/allbooks")
 def allbooks():
     return render_template("allbooks.html")
 
-
+#
 # add to buying cart
+#
 @app.route("/addtocart/<int:id>", methods=['GET', 'POST'])
 def add_to_buy(id):
     user_id = get_user().get_user_id()
@@ -579,7 +586,9 @@ def add_to_buy(id):
     print(cart_dict, "final database")
     return redirect(request.referrer)
 
+#
 # add to renting cart
+#
 @app.route("/addtorent/<int:id>", methods=['GET', 'POST'])
 def add_to_rent(id):
     user_id = get_user().get_user_id()
@@ -621,7 +630,9 @@ def add_to_rent(id):
     print(cart_dict, 'updated database')
     return redirect(request.referrer)
 
+#
 # view shopping cart
+#
 @app.route('/shopping_cart')
 def cart():
     user_id = get_user().get_user_id()
@@ -669,7 +680,9 @@ def cart():
                 total_price = float(("%.2f" % round(total_price, 2)))
     return render_template('cart.html', buy_count=buy_count, rent_count=rent_count, buy_cart=buy_cart, rent_cart=rent_cart, books_dict=books_dict, total_price=total_price)
 
+#
 # update quantity in buying cart
+#
 @app.route('/update_cart/<int:id>', methods=['GET', 'POST'])
 def update_cart(id):
     user_id = get_user().get_user_id()
@@ -689,8 +702,9 @@ def update_cart(id):
         cart_db.close()
     return redirect(request.referrer)
 
-
+#
 # delete item in buying cart
+#
 @app.route("/delete_buying_cart/<int:id>", methods=['GET', 'POST'])
 def delete_buying_cart(id):
     user_id = get_user().get_user_id()
@@ -711,8 +725,9 @@ def delete_buying_cart(id):
     cart_db.close()
     return redirect(request.referrer)
 
-
+#
 # delete item in renting cart
+#
 @app.route("/delete_renting_cart/<int:id>", methods=['GET', 'POST'])
 def delete_renting_cart(id):
     user_id = get_user().get_user_id()
@@ -730,8 +745,9 @@ def delete_renting_cart(id):
     cart_db.close()
     return redirect(request.referrer)
 
-
-# Checkout
+#
+# Checkout Form (for shipping address etc)
+#
 @app.route("/checkout", methods=['GET', 'POST'])
 def checkout():
     user_id = get_user().get_user_id()
@@ -780,7 +796,9 @@ def checkout():
     Orderform = OrderForm.OrderForm(request.form)
     return render_template("checkout.html", form=Orderform, total_price=total_price, buy_count=buy_count, rent_count=rent_count, buy_cart=buy_cart, rent_cart=rent_cart, books_dict=books_dict)
 
-
+#
+# Create Check out session with Stripe
+#
 @app.route('/create-checkout-session/<total_price>', methods=['POST'])
 def create_checkout_session(total_price):
     user_id = get_user().get_user_id()
@@ -963,6 +981,11 @@ def edit_status(order_id):
     db.close()
     print(order.get_order_status())
     return redirect(request.referrer)
+
+#
+# End of Chee Qing's Codes
+#
+
 
 #
 # Eden Pages
