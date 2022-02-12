ac7edd1d3ca55fd4894e132bcb962eac239ac9a2
SpeedFox198 <xstarx125@gmail.com>
2022-02-12 19:39:32 +0800

    fix js bug

--- static/js/manage_accounts.js
+++ static/js/manage_accounts.js
@@ -2,23 +2,23 @@
 
 const clickButton = id => document.getElementById(id).click();
 
-const displayUsername = document.getElementsByClassName("display-username");
-const formResetInputs = document.getElementsByClassName("reset-form");
+const displayUsername = [].slice.call(document.querySelectorAll('.display-username'));
+const formResetInputs = [].slice.call(document.querySelectorAll('.reset-form'));
 
 let selectedUserID = null;
 
 function selectAccount(userID, username, buttonId) {
-  selectedUserID = userID;
-  for (let i in displayUsername) {
-    displayUsername[i].innerText = username;
-  }
-  clickButton(buttonId);
+    selectedUserID = userID;
+    for (let i in displayUsername) {
+        displayUsername[i].innerText = username;
+    }
+    clickButton(buttonId);
 }
 
 function deleteUser() {
-  for (let i in formResetInputs) {
-    formResetInputs[i].click();
-  }
-  document.getElementById("deleteUserField").value = selectedUserID;
-  clickButton("deleteUserSubmit");
+    for (let i in formResetInputs) {
+        formResetInputs[i].click();
+    }
+    document.getElementById("deleteUserField").value = selectedUserID;
+    clickButton("deleteUserSubmit");
 }
