303d679ea7247c47f88a4cb9303202fc2831c974
Eden <64953425+Solaireis@users.noreply.github.com>
2022-02-13 19:52:58 +0800

    created coupon function

--- Cart.py
+++ Cart.py
@@ -1,4 +1,7 @@
 
+from webbrowser import get
+
+
 class SCart():
     buy_dict = {}
     rent_dict = []
@@ -9,18 +12,24 @@ class AddtoBuy(SCart):
         self.book_id = book_id
         self.book_quantity = book_quantity
         self.buy_dict.update({book_id:book_quantity})
+        
+    
 
+    
     def get_buy_dict(self):
         return self.buy_dict
 
     def get_book_id(self):
         return self.book_id
 
+
+
 class AddtoRent(SCart):
     def __init__(self, book_id):
         SCart.__init__(self)
         self.rent_dict.append(book_id)
         print(self.rent_dict)
+        
 
     def get_rent_dict(self):
         return self.rent_dict
@@ -28,4 +37,16 @@ class AddtoRent(SCart):
     def get_book_id(self):
         return self.book_id
 
+#eden integration, creates discount feature
+class Discount(SCart):
+    def __init__(self,discount):
+        SCart.__init__(self)
+        self.__discount = discount
+
+
+    def get_discount(self):
+        return self.__discount
+    
+    def set_discount(self,discount):
+        self.__discount = discount
 
